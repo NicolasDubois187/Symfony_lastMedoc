5f2c4af0172d3bdd3be4812d030ddfd2917c8075
NicolasDubois187 <nicolasdubois187.pro@gmail.com>
2022-03-28 14:30:34 +0200

    first_commit

---
+++ src/Entity/Blog.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BlogRepository::class)]
+class Blog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'date')]
+    private ?\DateTimeInterface $date;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $txt;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $img;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'blogs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+
+
+    public function __construct()
+    {
+        $this->User = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTxt(): ?string
+    {
+        return $this->txt;
+    }
+
+    public function setTxt(string $txt): self
+    {
+        $this->txt = $txt;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(?string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+
+
+
+}
---
+++ src/Form/BlogType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Blog;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('txt', TextareaType::class, [
+                'label' => 'Votre texte'
+            ])
+            ->add('img', FileType::class, [
+                'label' => 'votre illustration',
+                'mapped' => false,
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Blog::class,
+        ]);
+    }
+}
